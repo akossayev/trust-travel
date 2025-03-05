4b8eda8189e756f2830f3cdd4468ee44a38da5d8
Abay <abaykossayev@icloud.com>
2025-03-05 17:59:21 +0500

    feat: shadcn package

---
+++ packages/ui/tailwindcss.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "tailwindcss";
+
+export default {
+    content: [
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+            },
+        },
+    },
+    plugins: [require("tailwindcss-animate")],
+} satisfies Config;
