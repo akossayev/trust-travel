2716b9200d6132718e97db55fb617eb708dee0da
uxearwenvcmsqg <vladkim_88@icloud.com>
2025-03-15 13:00:50 +0500

    fix header

--- apps/frontend/client-landing/app/(landing)/_components/Header.tsx
+++ apps/frontend/client-landing/app/(landing)/_components/Header.tsx
@@ -27,7 +27,11 @@ export default function Header() {
           {isOpenTour ? <ArrowUpDark /> : <ArrowDownDark />}
         </li>
         <li className="text-sm lg:text-xl 2xl:text-2xl cursor-pointer">Для компании</li>
-        <li className="text-sm lg:text-xl 2xl:text-2xl cursor-pointer">Мой кабинет</li>
+        <li
+          className="text-sm lg:text-xl 2xl:text-2xl cursor-pointer"
+          onClick={() => setIsOpenOffice(!isOpenOffice)}>
+          Мой кабинет
+        </li>
       </ul>
       {isOpenTour && (
         <ul className="flex flex-col items-center gap-1 absolute left-[900px] top-[100px] w-[220px] h-[160px] p-2 rounded-[30px] bg-white/50 backdrop-blur-[30px] shadow-sm border-[1px] border-[#CACACA] text-[#747474]">
@@ -42,10 +46,15 @@ export default function Header() {
         </ul>
       )}
       {isOpenOffice && (
-        <ul className="flex flex-col">
-          <li>Войти</li>
-          <li>Зарегистрироваться</li>
-          <li>Бизнес-аккаунт</li>
+        <ul className="flex flex-col items-center gap-1 absolute left-[1240px] top-[100px] w-[220px] h-[160px] p-2 rounded-[30px] bg-white/50 backdrop-blur-[30px] shadow-sm border-[1px] border-[#CACACA] text-[#747474]">
+          {officeList.map((elem, index) => (
+            <li
+              key={index}
+              className={`py-1 px-5 rounded-[30px] w-full h-[50px] text-center pt-[10px] cursor-pointer ${activeTypeTour === index ? 'bg-foreground' : ''}`}
+              onClick={() => setActiveTypeTour(index)}>
+              {elem}
+            </li>
+          ))}
         </ul>
       )}
       <BurgerMenuMobile className="lg:hidden" />
