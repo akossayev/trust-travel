c6161e510248c53b61d031958bec0cef9c675dfb
uxearwenvcmsqg <vladkim_88@icloud.com>
2025-03-23 03:18:49 +0500

    update attraction page (decstop landing)

--- apps/frontend/client-landing/app/attractionsPage/page.tsx
+++ apps/frontend/client-landing/app/attractionsPage/page.tsx
@@ -5,8 +5,8 @@ export default function page() {
   return (
     <div className="mt-[60px]">
       <div className="flex flex-col items-center">
-        <h2 className="text-center">Достопримечательности</h2>
-        <div className="flex flex-col items-center">
+        <h2 className="text-center lg:text-2xl lg:font-medium">Достопримечательности</h2>
+        <div className="flex flex-col items-center ">
           <SoloAttraction />
           <SoloAttraction />
           <SoloAttraction />
